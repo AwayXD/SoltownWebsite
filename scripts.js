0dcc205c39420e273a0d86231c36c0b279a6a48f
Away <125528637+AwayXD@users.noreply.github.com>
2024-12-04 14:19:39 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Fade-in effect for elements with class '.fade-in'
     const elements = document.querySelectorAll('.fade-in');
-    const options = {
+    const fadeOptions = {
         root: null,
         rootMargin: '0px',
         threshold: 0.1
     };
-    const observer = new IntersectionObserver((entries, observer) => {
+
+    const fadeObserver = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('show');
                 observer.unobserve(entry.target);
             }
         });
-    }, options);
+    }, fadeOptions);
+
     elements.forEach(el => {
-        observer.observe(el);
+        fadeObserver.observe(el);
+    });
+
+    // Intersection Observer for lazy-loading images with fade-in effect
+    const images = document.querySelectorAll('.image-hover');
+    const imageOptions = {
+        threshold: 0.3
+    };
+
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('loaded');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, imageOptions);
+
+    images.forEach(image => {
+        imageObserver.observe(image);
     });
 });
