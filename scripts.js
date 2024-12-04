8a13340e79582b9638387ed31a0a47b48fe3e533
Away <125528637+AwayXD@users.noreply.github.com>
2024-12-04 12:14:49 -0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.fade-in');
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    elements.forEach(el => {
+        observer.observe(el);
+    });
+});
