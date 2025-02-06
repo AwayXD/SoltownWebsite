2768dfc705a68833432df5620354d4e7f6275955
Astrid <catgirlykyla@proton.me>
2025-02-06 13:23:12 -0600

    test 3

--- scripts.js
+++ scripts.js
@@ -9,11 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     function toggleMenu() {
         var menu = document.getElementById("mobileMenu");
-        if (menu.style.display === "flex") {
-            menu.style.display = "none";
-        } else {
-            menu.style.display = "flex";
-        }
+        menu.classList.toggle("open");
     }
 
     const fadeObserver = new IntersectionObserver((entries, observer) => {
